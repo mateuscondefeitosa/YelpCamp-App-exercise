83e66dfebb2eb75dbe76efc5af0771bafc301e76
Mateus Conde <devmateuscondefeitosa@gmail.com>
2021-07-26 17:15:38 -0300

    Add models paste and connected MONGODB

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
-const app = express();
 const path = require('path')
+const mongoose = require('mongoose');
+
+
+// CONECTA COM O MONGODB
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+// VERIFICA SE HÁ ERRO DE CONECÇÃO COM O MONGODB
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=>{
+    console.log("Database Connected!");
+});
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
