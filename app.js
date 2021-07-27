78b9242a0aea3c237af933a0ac83e4ab9ee30201
Mateus Conde <devmateuscondefeitosa@gmail.com>
2021-07-27 10:29:46 -0300

    Add Create esqueleton

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
+const Campground = require('./models/campground')
 
 
 // CONECTA COM O MONGODB
@@ -13,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 // VERIFICA SE HÁ ERRO DE CONECÇÃO COM O MONGODB
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("Database Connected!");
 });
 
@@ -21,12 +22,44 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.send('YELPPPP');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+});
+
+
+// PARA CRIAR UM NOVO ITEN (C)
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campgrounds/new');
 })
 
-app.listen(3000, ()=>{
+app.post('/campgrounds', async (req,res)=>{
+// NECESSARIO PARSE  (app.use)
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+// Procurar por ID (R)
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
